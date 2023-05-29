fe62a96358cf1a50f4687c547860a4a6f177c5f1
AungPaingSoe <aungpaingsoe097238@gmail.com>
2023-05-29 10:26:36 +0630

    namespace

---
+++ namespace/Form.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Html;
+
+class Form 
+{
+    public function form()
+    {
+        return "<form></form>";
+    }
+}
---
+++ namespace/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Html\Form;
+
+require_once "./Form.php";
+
+$form = new Form;
+
+var_dump($form->form());
